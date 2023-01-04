a72004bdeb02bf17f2319043473aa778c5a5e8de
dvd_mcf <dave@techia.com>
2023-01-04 11:38:51 +0000

    alt upload

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ async function setup() {
 	
 	// DEVICE 1
 	
-    const patchExportURL1 = "export/patch1.export.json";
+    const patchExportURL1 = "export/patch4.export.json";
     
     // Fetch the exported patcher
     let response, patcher;
@@ -108,7 +108,7 @@ async function setup() {
 console.log("Device 1 created.")
 // DEVICE 2
 
-    const patchExportURL2 = "export/patch3.export.json";
+    const patchExportURL2 = "export/patch1.export.json";
     patcher = null
     // Fetch the exported patcher
 //    let response, patcher;
